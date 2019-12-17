7ff95abfce488a227b001b825ee8c7261381f35e
David Turnes <d3turnes@gmail.com>
2019-12-17 13:32:01 +0100

    updated helpers

--- helpers.php
+++ helpers.php
@@ -1,7 +1,7 @@
 <?php
 
-if (! function_exists('template_include') ):
+if (!function_exists('template_include')) {
 	public function template_include($template, $data = array()) {
 		return D3turnes\Helpers\Template::render($template, $data);
 	}
-endif;
+}
